8b146bf6e767fe886f8190cebcb1a2077fe3164b
JukemJ <chris@dixn.dev>
2023-05-03 18:28:10 -0700

    implemented features

--- src/scripts.js
+++ src/scripts.js
@@ -9,13 +9,50 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentImageIndex = 0;
 
   // TODO: (Issue #1) Add an event listener for each image in the gallery to show it in the modal when clicked
-
+  for(let i = 0; i < images.length; i++){
+    images[i].addEventListener('click', () => {
+      images[i].classList.toggle('modal-image')
+      modal.classList.remove('hidden')
+      modalImage.src = images[i].src
+      currentImageIndex = i
+      if(i == 0) prevBtn.setAttribute('disabled',true)
+      if(i == images.length -1) nextBtn.setAttribute('disabled',true)
+    })
+  }
   // TODO: (Issue #2) Add event listeners for the prevBtn and nextBtn for modal navigation functionality
+  nextBtn.addEventListener('click', () => {
+    let i = currentImageIndex
+    if(i == 0) prevBtn.removeAttribute('disabled')
+    images[i].classList.toggle('modal-image')
+    i++
+    images[i].classList.toggle('modal-image')
+    modalImage.src = images[i].src
+    currentImageIndex = i
+    if(i == images.length -1) nextBtn.setAttribute('disabled',true)
+  })
+  prevBtn.addEventListener('click', () => {
+    if(currentImageIndex == 0) return
+    let i = currentImageIndex
+    if(i == images.length - 1) nextBtn.removeAttribute('disabled')
+    
+    images[i].classList.toggle('modal-image')
+    i--
+    images[i].classList.toggle('modal-image')
+    modalImage.src = images[i].src
+    currentImageIndex = i
+    if(i == 0) prevBtn.setAttribute('disabled',true)
+  })
+
   // Clean Code Hint: Issue #1 and #2 both deal with updating the modal image based on currentImageIndex. Consider creating a function to handle this functionality.
 
   // TODO: (Issue #3) Add an event listener for the closeBtn to close the modal
+  closeBtn.addEventListener('click', () => {
+    modal.classList.add('hidden')
+  })
 
   // TODO: (Issue #3) Add an event listener to close the modal when clicking the modal background
-
+  modal.addEventListener('click', () => {
+    modal.classList.add('hidden')
+  })
   // TODO: (Issue #4) Create a function to update the state of the navigation buttons based on currentImageIndex
 });
