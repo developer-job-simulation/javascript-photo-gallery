07a299a85a056651c745cd163fa262d568c742be
Awinti Amin <awintiamine@gmail.com>
2023-05-12 21:40:03 +0100

    fix photo gallery issues

--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,79 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById("modal");
-  const modalImage = document.getElementById("modalImage");
-  const closeBtn = document.getElementById("closeBtn");
-  const prevBtn = document.getElementById("prevBtn");
-  const nextBtn = document.getElementById("nextBtn");
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modalImage');
+  const closeBtn = document.getElementById('closeBtn');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
 
-  const images = Array.from(document.querySelectorAll(".image"));
+  const images = Array.from(document.querySelectorAll('.image'));
   let currentImageIndex = 0;
+  let imageName;
 
-  // TODO: (Issue #1) Add an event listener for each image in the gallery to show it in the modal when clicked
+  function disableNavBtnsOnEdges(index) {
+    if (index === 0) prevBtn.disabled = true;
+    else if (index === 11) nextBtn.disabled = true;
+    else {
+      prevBtn.disabled = false;
+      nextBtn.disabled = false;
+    }
+  }
 
-  // TODO: (Issue #2) Add event listeners for the prevBtn and nextBtn for modal navigation functionality
-  // Clean Code Hint: Issue #1 and #2 both deal with updating the modal image based on currentImageIndex. Consider creating a function to handle this functionality.
+  images.forEach((image) =>
+    image.addEventListener('click', (event) => {
+      modal.classList.remove('hidden');
+      modalImage.src = image.src;
 
-  // TODO: (Issue #3) Add an event listener for the closeBtn to close the modal
+      imageName = modalImage.src.slice(modalImage.src.lastIndexOf('/') + 1);
 
-  // TODO: (Issue #3) Add an event listener to close the modal when clicking the modal background
+      let imageIndex = parseInt(imageName.replace(/[^0-9]/g, ''));
 
-  // TODO: (Issue #4) Create a function to update the state of the navigation buttons based on currentImageIndex
+      if (!isNaN(imageIndex)) currentImageIndex = imageIndex;
+      else currentImageIndex = 0;
+      disableNavBtnsOnEdges(currentImageIndex);
+    })
+  );
+
+  nextBtn.addEventListener('click', (event) => {
+    imageName = modalImage.src.slice(modalImage.src.lastIndexOf('/') + 1);
+
+    if (currentImageIndex !== 11) {
+      let nextImageName = imageName.replace(/\d+/, (n) => ++n);
+
+      if (!/\d/.test(imageName)) nextImageName = 'image-1.jpg';
+
+      modalImage.src = '/public/' + nextImageName;
+      currentImageIndex++;
+    }
+
+    disableNavBtnsOnEdges(currentImageIndex);
+  });
+
+  prevBtn.addEventListener('click', (event) => {
+    imageName = modalImage.src.slice(modalImage.src.lastIndexOf('/') + 1);
+
+    let prevImageName = 'image.jpg';
+
+    if (currentImageIndex > 1) {
+      prevImageName = imageName.replace(/\d+/, (n) => --n);
+    }
+
+    currentImageIndex--;
+    modalImage.src = '/public/' + prevImageName;
+
+    disableNavBtnsOnEdges(currentImageIndex);
+  });
+
+  closeBtn.addEventListener('click', (event) => {
+    modal.classList.add('hidden');
+  });
+
+  modal.addEventListener('click', (event) => {
+    if (
+      event.target != modalImage &&
+      event.target != nextBtn &&
+      event.target != prevBtn
+    ) {
+      modal.classList.add('hidden');
+    }
+  });
 });
