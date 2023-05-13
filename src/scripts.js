c938a21a01e91fd9b395878866c93d828c6f5b21
Awinti Amin <awintiamine@gmail.com>
2023-05-13 14:03:32 +0100

    fixing photo gallery issues

--- src/scripts.js
+++ src/scripts.js
@@ -7,59 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const images = Array.from(document.querySelectorAll('.image'));
   let currentImageIndex = 0;
-  let imageName;
-
-  function disableNavBtnsOnEdges(index) {
-    prevBtn.disabled = false;
-    nextBtn.disabled = false;
-
-    if (index === 0) prevBtn.disabled = true;
-    else if (index === 11) nextBtn.disabled = true;
-  }
 
   images.forEach((image) =>
     image.addEventListener('click', (event) => {
       modal.classList.remove('hidden');
       modalImage.src = image.src;
 
-      imageName = modalImage.src.slice(modalImage.src.lastIndexOf('/') + 1);
-
-      let imageIndex = parseInt(imageName.replace(/[^0-9]/g, ''));
-
-      if (!isNaN(imageIndex)) currentImageIndex = imageIndex;
-      else currentImageIndex = 0;
-      disableNavBtnsOnEdges(currentImageIndex);
+      currentImageIndex = images.indexOf(image);
+      updateModalNavigation(currentImageIndex);
     })
   );
 
   nextBtn.addEventListener('click', (event) => {
-    imageName = modalImage.src.slice(modalImage.src.lastIndexOf('/') + 1);
-
     if (currentImageIndex !== 11) {
-      let nextImageName = imageName.replace(/\d+/, (n) => ++n);
-
-      if (!/\d/.test(imageName)) nextImageName = 'image-1.jpg';
-
-      modalImage.src = '/public/' + nextImageName;
-      currentImageIndex++;
+      modalImage.src = images[++currentImageIndex].src;
     }
-
-    disableNavBtnsOnEdges(currentImageIndex);
+    updateModalNavigation(currentImageIndex);
   });
 
   prevBtn.addEventListener('click', (event) => {
-    imageName = modalImage.src.slice(modalImage.src.lastIndexOf('/') + 1);
-
-    let prevImageName = 'image.jpg';
-
-    if (currentImageIndex > 1) {
-      prevImageName = imageName.replace(/\d+/, (n) => --n);
+    if (currentImageIndex !== 0) {
+      modalImage.src = images[--currentImageIndex].src;
     }
 
-    currentImageIndex--;
-    modalImage.src = '/public/' + prevImageName;
-
-    disableNavBtnsOnEdges(currentImageIndex);
+    updateModalNavigation(currentImageIndex);
   });
 
   closeBtn.addEventListener('click', (event) => {
@@ -75,4 +46,12 @@ document.addEventListener('DOMContentLoaded', () => {
       modal.classList.add('hidden');
     }
   });
+
+  function updateModalNavigation(index) {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+
+    if (index === 0) prevBtn.disabled = true;
+    else if (index === 11) nextBtn.disabled = true;
+  }
 });
