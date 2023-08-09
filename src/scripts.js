0f145cc62f7aa2a8cea9b647f64ffc6c4dc1de8e
patricia-oliveira <patriciia.mota@gmail.com>
2023-08-09 10:52:06 +0100

    Close the modal when the close button is clicked and when clicking the modal background

--- src/scripts.js
+++ src/scripts.js
@@ -32,6 +32,16 @@ function addInteractivityNextButton() {
   });
 }
 
+function activateModalClose() {
+  closeBtn.addEventListener("click", (e) => {
+    modal.classList.add("hidden");
+  });
+
+  window.addEventListener("click", (event) => {
+    return event.target === modal ? modal.classList.add("hidden") : null;
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("modal");
   const modalImage = document.getElementById("modalImage");
@@ -42,10 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
   activateImageModal();
   addInteractivityPreviousButton();
   addInteractivityNextButton();
-
-  // TODO: (Issue #3) Add an event listener for the closeBtn to close the modal
-
-  // TODO: (Issue #3) Add an event listener to close the modal when clicking the modal background
+  activateModalClose();
 
   // TODO: (Issue #4) Create a function to update the state of the navigation buttons based on currentImageIndex
 });
