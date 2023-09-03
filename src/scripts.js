4c7e49ceba3e880c9f3732351cddef137c636bbd
Ralph <ralphorleynes@gmail.com>
2023-09-03 23:53:02 +0800

    rearranged

--- src/scripts.js
+++ src/scripts.js
@@ -24,6 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  // TODO: (Issue #2) Add event listeners for the prevBtn and nextBtn for modal navigation functionality
+
 // Add click event listener for the "prev" button
 prevBtn.addEventListener("click", () => {
   updateModalWithImage(currentImageIndex - 1); // Show the previous image
@@ -33,6 +35,26 @@ prevBtn.addEventListener("click", () => {
 nextBtn.addEventListener("click", () => {
   updateModalWithImage(currentImageIndex + 1); // Show the next image
 });
+
+
+// TODO: (Issue #3) Add an event listener for the closeBtn to close the modal
+
+  // Add click event listener to close the modal when the closeBtn is clicked
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none"; // Close the modal
+  });
+
+
+  // TODO: (Issue #3) Add an event listener to close the modal when clicking the modal background
+  
+  // Add click event listener for the modal background to close the modal
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none"; // Close the modal
+  }
+});
+
+  // TODO: (Issue #4) Create a function to update the state of the navigation buttons based on currentImageIndex
 
 // Function to update the modal with the image at the specified index
 function updateModalWithImage(index) {
@@ -49,28 +71,13 @@ function updateModalWithImage(index) {
   nextBtn.disabled = currentImageIndex === images.length - 1;
 }
 
-  // Add click event listener to close the modal when the closeBtn is clicked
-  closeBtn.addEventListener("click", () => {
-    modal.style.display = "none"; // Close the modal
-  });
+});
+
 
   
-  // Add click event listener for the modal background to close the modal
-modal.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none"; // Close the modal
-  }
-});
-
-});
-
-
-  // TODO: (Issue #2) Add event listeners for the prevBtn and nextBtn for modal navigation functionality
   // Clean Code Hint: Issue #1 and #2 both deal with updating the modal image based on currentImageIndex. Consider creating a function to handle this functionality.
 
-  // TODO: (Issue #3) Add an event listener for the closeBtn to close the modal
+  
 
-  // TODO: (Issue #3) Add an event listener to close the modal when clicking the modal background
 
-  // TODO: (Issue #4) Create a function to update the state of the navigation buttons based on currentImageIndex
 
